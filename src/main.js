b51fca32f1c521a8767ec99d9d36ee23446db392
chen jie <jie.chen@shui-inc.com>
2020-10-10 14:49:25 +0800

    添加less、iview

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import ViewUI from 'view-design';
+import '../src/assets/css/theme.less';
 
-Vue.config.productionTip = false
+Vue.use(ViewUI, {
+    transfer: true,
+    size: 'small',
+    capture: false,
+});
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: h => h(App),
+}).$mount('#app');
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    publicPath: '/',
+    css: {
+        loaderOptions: {
+            less: {
+                lessOptions: {
+                    javascriptEnabled: true,
+                },
+            },
+        },
+    },
+};
